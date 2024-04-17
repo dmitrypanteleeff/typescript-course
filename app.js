042f2441b999f7ac0da4e6cda155a43da618122f
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2024-04-17 22:45:52 +0300

    017 Enums

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-"use strict";
-const skills = [1, 'Dev'];
-const skillsArray = ['Dev', 'DevOps'];
-const skills2 = ['Dev', 'DevOps'];
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode["IN_PROGRESS"] = "in progress";
+    StatusCode["FAILED"] = "failed";
+})(StatusCode || (StatusCode = {}));
+var res = {
+    message: 'Платёж успешен',
+    statusCode: StatusCode.SUCCESS
+};
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+// 1 - успех
+// 2 - в процессе
+// 3 - отклонён
+function action(status) {
+}
+action(StatusCode.SUCCESS);
+action(1);
+function test(x) {
+}
+var res2 = 3 /* Roles.USER */;
+console.log(res2);
+//если это обычный enum, то это функция, если это const enum, то это преобразовывается в константу
+// и подставляется в те места, где было обращение к этому enum
--- app.ts
+++ app.ts
@@ -1,4 +1,41 @@
-const skills: readonly [number, string] = [1, 'Dev'];
-const skillsArray: readonly string[] = ['Dev', 'DevOps'];
-const skills2: ReadonlyArray<string> = ['Dev', 'DevOps'];
+enum StatusCode {
+    SUCCESS = 1,
+    IN_PROGRESS = 'in progress',
+    FAILED = 'failed'
+}
 
+const res = {
+    message: 'Платёж успешен',
+    statusCode: StatusCode.SUCCESS
+}
+
+
+if (res.statusCode === StatusCode.SUCCESS) {
+
+}
+
+// 1 - успех
+// 2 - в процессе
+// 3 - отклонён
+
+function action(status: StatusCode) {
+
+}
+
+action(StatusCode.SUCCESS);
+action(1);
+
+const enum Roles {
+    ADMIN = 1, 
+    USER = ADMIN + 2
+}
+
+function test(x: {ADMIN: number}) {
+
+}
+
+const res2 = Roles.USER
+console.log(res2)
+
+//если это обычный enum, то это функция, если это const enum, то это преобразовывается в константу
+// и подставляется в те места, где было обращение к этому enum
