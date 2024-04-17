50402fce6decc0bbdcb971914f848910aa3b70dd
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2024-04-17 23:03:37 +0300

    018 - Упражнение

--- app.ts
+++ app.ts
@@ -1,41 +1,22 @@
-enum StatusCode {
-    SUCCESS = 1,
-    IN_PROGRESS = 'in progress',
-    FAILED = 'failed'
+
+const enum Status {
+    PUBLISHED = 'published',
+    DRAFT = 'draft',
+    DELETED = 'deleted'
+} 
+
+
+async function getFaqs(req: {topicId: number, status?: Status}): Promise<Array<{
+    question: string,
+	answer: string,
+	tags: string[],
+	likes: number,
+	status: Status
+}>> {
+	const res = await fetch('/faqs', {
+		method: 'POST',
+		body: JSON.stringify(req)
+	});
+	const data = await res.json();
+	return data;
 }
-
-const res = {
-    message: 'Платёж успешен',
-    statusCode: StatusCode.SUCCESS
-}
-
-
-if (res.statusCode === StatusCode.SUCCESS) {
-
-}
-
-// 1 - успех
-// 2 - в процессе
-// 3 - отклонён
-
-function action(status: StatusCode) {
-
-}
-
-action(StatusCode.SUCCESS);
-action(1);
-
-const enum Roles {
-    ADMIN = 1, 
-    USER = ADMIN + 2
-}
-
-function test(x: {ADMIN: number}) {
-
-}
-
-const res2 = Roles.USER
-console.log(res2)
-
-//если это обычный enum, то это функция, если это const enum, то это преобразовывается в константу
-// и подставляется в те места, где было обращение к этому enum
