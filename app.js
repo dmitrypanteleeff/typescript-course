6b2976e5da4dd0f6679d1666886a1ee3b09e0cc3
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2024-04-24 13:57:56 +0300

    031 - Приведение типов

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 "use strict";
-const n = null;
-const n1 = null;
-function getUser() {
-    if (Math.random() > 0.5) {
-        return null;
-    }
-    else {
-        return {
-            name: 'hayley'
-        };
-    }
-}
-const user = getUser();
-if (user) {
-    const n55 = user.name;
+let a = 5;
+let b = a.toString();
+let e = new String(a).valueOf();
+let f = new Boolean(a).valueOf();
+let c = 'weqwdqw';
+let d = +c;
+let dd = parseInt(c);
+const user = {
+    name: 'Вася',
+    email: 'Vasya@yandex.ru',
+    login: 'Vasia'
+};
+// const admin: IAdmin = {
+//     ...user,
+//     role: 1
+// }
+//const admin: IAdmin = user;
+function userToAdmin(user) {
+    return {
+        name: user.name,
+        role: 1
+    };
 }
--- app.ts
+++ app.ts
@@ -1,25 +1,39 @@
-const n: null = null;
-const n1: any = null;
-// const n2: number = null;
-// const n3: string = null;
-// const n4: boolean = null;
+let a = 5;
+let b: string = a.toString();
+let e: string = new String(a).valueOf();
+let f: boolean = new Boolean(a).valueOf();
+
+let c = 'weqwdqw';
+let d: number = +c;
+let dd: number = parseInt(c);
 
 interface IUser {
-    name: string;
+    name: string,
+    email: string,
+    login: string
 }
 
-function getUser() {
-    if (Math.random() > 0.5) {
-        return null;
-    }
-    else {
-        return {
-            name: 'hayley'
-        } as IUser
+const user: IUser = {
+    name: 'Вася',
+    email: 'Vasya@yandex.ru',
+    login: 'Vasia'
+}
+
+interface IAdmin {
+    name: string,
+    role: number
+}
+
+// const admin: IAdmin = {
+//     ...user,
+//     role: 1
+// }
+//const admin: IAdmin = user;
+
+function userToAdmin(user: IUser): IAdmin {
+    return {
+        name: user.name,
+        role: 1
     }
 }
 
-const user = getUser();
-if (user) {
-    const n55 = user.name;
-}
