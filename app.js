9163d45bfbcb9b2aadc622cd80fb63b9e5d8a7f4
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2024-04-26 13:39:33 +0300

    036 - Конструкторы

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 "use strict";
+/*
+
+*/
 class User {
-    constructor(name) {
-        this.name = name;
+    constructor(ageOrName, age) {
+        if (typeof ageOrName === 'string') {
+            this.name = ageOrName;
+        }
+        if (typeof ageOrName === 'number') {
+            this.age = ageOrName;
+        }
+        if (typeof age === 'number') {
+            this.age = age;
+        }
     }
 }
 const user = new User('Vasya');
-console.log(user);
-user.name = 'Petya';
-console.log(user);
-class Admin {
-}
-const admin = new Admin();
-admin.role = 1;
+const user2 = new User();
+const user3 = new User(33);
+const user4 = new User('Ura', 33);
--- app.ts
+++ app.ts
@@ -1,20 +1,33 @@
+/*
+	Конструтор - это функция, которая конструирует, возвращает наш объект
+	Она не может быть переопределена с точки зрения возвращаемого типа
+	Мы не можем исопользовать Дженерики и проставлять явный тип
+	Но мы можем делать оверроуд, тем самым объявляя различные вариации 
+	нашего конструктора для класса
+*/
+
 class User {
 	name: string;
+	age: number;
 
-	constructor(name: string) {
-		this.name = name;
+	constructor();
+	constructor(name: string);
+	constructor(age: number);
+	constructor(name: string, age: number);
+	constructor(ageOrName?: string | number, age?: number) {
+		if (typeof ageOrName === 'string') {
+			this.name = ageOrName;
+		}
+		if (typeof ageOrName === 'number') {
+			this.age = ageOrName;
+		} 
+		if (typeof age === 'number') {
+			this.age = age;
+		} 
 	}
 }
 
 const user = new User('Vasya');
-console.log(user);
-user.name = 'Petya';
-console.log(user)
-
-class Admin {
-	role: number;
-	//role!: number;
-}
-
-const admin = new Admin();
-admin.role = 1;
+const user2 = new User();
+const user3 = new User(33)
+const user4 = new User('Ura',33)
