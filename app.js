23065ac2c33e7519f3b94f6c13afe7d440a0096f
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2024-05-03 09:46:57 +0300

    043 Композиция против наследования

--- app.js
+++ app.js
@@ -1,47 +1,45 @@
 "use strict";
-class Payment {
-    constructor(id) {
-        this.status = 'new';
-        this.id = id;
-    }
-    pay() {
-        this.status = 'paid';
-    }
-}
-class PersistedPayment extends Payment {
-    constructor() {
-        const id = Math.random();
-        super(id);
-    }
-    save() {
-        // Сохраняет в базу
-    }
-    pay(date) {
-        //this.status = 'paid';
-        super.pay();
-        if (date) {
-            this.paidAt = date;
-        }
-    }
-}
-new PersistedPayment();
+/*
+    Наследование - extends - приходится тащить всюду
+    Композция - позволяет не нарушать объекты.
+    У объектов внутри композиции свои отдельные доменные области
+    
+    Наследование лучше использовать если мы наследуемся
+    в рамках одной домменной области.
+
+    Когда не нужно делать, когда наследуемся от сложных внутренних массивов
+    Не следует наследоваться, если пересекаем границу одной домменной области
+    и уходим в другую домменную область - пример с UserWithPayment2
+*/
 class User {
-    constructor() {
-        this.name = 'user';
-        console.log(this.name);
+    constructor(name) {
+        this.name = name;
     }
 }
-class Admin extends User {
-    constructor() {
-        super();
-        this.name = 'admin';
-        console.log(this.name);
+class Users extends Array {
+    searchByName(name) {
+        return this.filter(u => u.name === name);
+    }
+    toString() {
+        return this.map(u => u.name).join(', ');
     }
 }
-new Admin();
-class HttpError extends Error {
-    constructor(message, code) {
-        super(message);
-        this.code = code !== null && code !== void 0 ? code : 500;
+const users = new Users();
+users.push(new User('Вася'));
+users.push(new User('Петя'));
+console.log(users.toString());
+class UserList {
+    push(u) {
+        this.users.push(u);
+    }
+}
+class Payment {
+}
+class UserWithPayment extends Payment {
+}
+class UserWithPayment2 {
+    constructor(user, payment) {
+        this.payment = payment;
+        this.user = user;
     }
 }
--- app.ts
+++ app.ts
@@ -1,73 +1,63 @@
+/*
+	Наследование - extends - приходится тащить всюду
+	Композция - позволяет не нарушать объекты. 
+	У объектов внутри композиции свои отдельные доменные области
+	
+	Наследование лучше использовать если мы наследуемся 
+	в рамках одной домменной области.
 
-type PaymentStatus = 'new' | 'paid';
+	Когда не нужно делать, когда наследуемся от сложных внутренних массивов
+	Не следует наследоваться, если пересекаем границу одной домменной области
+	и уходим в другую домменную область - пример с UserWithPayment2
+*/
 
-class Payment {
-	id: number;
-	status: PaymentStatus = 'new';
-
-	constructor(id: number) {
-		this.id = id;
-	}
-
-	pay() {
-		this.status = 'paid';
-	}
-}
-
-class PersistedPayment extends Payment {
-	databaseId: number;
-	paidAt: Date;
-
-	constructor() {
-		const id = Math.random();
-		super(id);
-	}
-
-	save() {
-		// Сохраняет в базу
-	}
-
-	override pay(date?: Date) { // override метод
-		//this.status = 'paid';
-		super.pay();
-		if (date) {
-			this.paidAt = date;
-		}
-	}
-	// pay(date?: Date) {
-	// 	//this.status = 'paid';
-	// 	super.pay();
-	// 	if (date) {
-	// 		this.paidAt = date;
-	// 	}
-	// }
-}
-
-new PersistedPayment()
 
 class User {
-	name: string = 'user';
+	name: string;
 
-	constructor() {
-		console.log(this.name)
+	constructor(name: string) {
+		this.name = name;
 	}
 }
 
-class Admin extends User {
-	name: string = 'admin';
+class Users extends Array<User> {
 
-	constructor() {
-		super();
-		console.log(this.name);
+	searchByName(name: string) {
+		return this.filter(u => u.name === name);
+	}
+
+	override toString(): string {
+		return this.map(u => u.name).join(', ');
 	}
 }
 
-new Admin();
+const users = new Users();
+users.push(new User('Вася'));
+users.push(new User('Петя'));
+console.log(users.toString());
 
-class HttpError extends Error {
-	code: number;
-	constructor(message: string, code?: number) {
-		super(message);
-		this.code = code ?? 500;
+class UserList {
+	users: User[];
+
+	push(u: User) {
+		this.users.push(u);
+	}
+}
+
+class Payment {
+	date: Date;
+}
+
+class UserWithPayment extends Payment {
+	name: string;
+}
+
+class UserWithPayment2 {
+	user: User;
+	payment: Payment;
+
+	constructor(user: User, payment: Payment) {
+		this.payment = payment;
+		this.user = user;
 	}
 }
