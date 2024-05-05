557b6f12343fbdb657c11b59a8bdc9cbc6f67479
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2024-05-05 23:36:32 +0300

    046 Статические свойства

--- app.js
+++ app.js
@@ -1,72 +1,34 @@
 "use strict";
-class Product {
-    constructor(id, name, cost) {
-        this.id = id;
-        this.name = name;
-        this.cost = cost;
+/*
+    Статические свойства не требуют создание инстанса класса
+    К свойствам можно обращаться без создания инстанса
+
+    Любой статичный метод может быть асинхронным
+    Инициализатор static не может быть асинхронным. Но может задавать изначальные значения
+*/
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+class UserService {
+    static getUser(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return UserService.db.findById(id);
+        });
+    }
+    constructor(id) {
+    }
+    create() {
+        UserService.db = 'asd';
     }
 }
-class Delivery {
-    constructor(date) {
-        this.date = date;
-    }
-}
-class HomeDelivery extends Delivery {
-    constructor(date, address) {
-        super(date);
-        this.date = date;
-        this.address = address;
-    }
-}
-class StoreDelivery extends Delivery {
-    constructor(date, storeID) {
-        super(new Date());
-        this.date = date;
-        this.storeID = storeID;
-    }
-}
-class Cart {
-    constructor() {
-        this.products = [];
-    }
-    addProduct(product) {
-        this.products.push(product);
-        console.log(this.products);
-        return this.products;
-    }
-    removeProduct(id) {
-        this.products = this.products.filter((product) => product.id !== id);
-        console.log(this.products);
-        return this.products;
-    }
-    finalCost() {
-        const sum = this.products.reduce((acc, product) => acc + product.cost, 0);
-        console.log(sum);
-        return sum;
-    }
-    setDelivery(delivery) {
-        this.delivery = delivery;
-        console.log(this.delivery);
-    }
-    checkOut() {
-        this.products.length > 0 && this.delivery !== undefined;
-        if (this.products.length === 0) {
-            throw new Error('Нет продуктов');
-        }
-        if (!this.delivery) {
-            throw new Error('Не выбрали доставку');
-        }
-        return { success: true };
-    }
-}
-const cart = new Cart();
-const product1 = new Product(1, 'Печенье', 100);
-const product2 = new Product(2, 'Торт', 300);
-const product3 = new Product(3, 'Молоко', 500);
-cart.addProduct(product1);
-cart.addProduct(product2);
-cart.addProduct(product3);
-cart.removeProduct(2);
-cart.setDelivery(new HomeDelivery(new Date(), 'Москва'));
-cart.finalCost();
-cart.checkOut();
+(() => {
+})();
+UserService.getUser(1);
+const inst = new UserService(1);
+inst.create();
--- app.ts
+++ app.ts
@@ -1,84 +1,31 @@
-class Product {
-	constructor(
-		public id: number,
-		public name: string,
-		public cost: number
-	) { }
-}
+/*
+	Статические свойства не требуют создание инстанса класса
+	К свойствам можно обращаться без создания инстанса
 
-class Delivery {
-	constructor(
-		public date: Date
-	) { }
-}
+	Любой статичный метод может быть асинхронным
+	Инициализатор static не может быть асинхронным. Но может задавать изначальные значения
+*/
 
-class HomeDelivery extends Delivery {
-	constructor(
-		public date: Date,
-		public address: string
-	) {
-		super(date);
+class UserService {
+	static db: any;
+
+	static async getUser(id: number) {
+		return UserService.db.findById(id);
+	}
+
+	constructor(id: number) {
+
+	}
+
+	create() {
+		UserService.db = 'asd';
+	}
+
+	static {
+
 	}
 }
 
-class StoreDelivery extends Delivery {
-	constructor(
-		public date: Date,
-		public storeID: number
-	) {
-		super(new Date());
-	}
-}
-
-type DeliveryOptions = HomeDelivery | StoreDelivery;
-
-class Cart {
-	private products: Array<Product> = [];
-	private delivery: DeliveryOptions;
-
-	addProduct(product: Product): Product[] {
-		this.products.push(product);
-		console.log(this.products);
-		return this.products;
-	}
-
-	removeProduct(id: number): Product[] {
-		this.products = this.products.filter((product: Product) => product.id !== id);
-		console.log(this.products);
-		return this.products;
-	}
-
-	finalCost(): number {
-		const sum = this.products.reduce((acc, product) => acc + product.cost, 0);
-		console.log(sum);
-		return sum;
-	}
-
-	setDelivery(delivery: DeliveryOptions): void {
-		this.delivery = delivery;
-		console.log(this.delivery);
-	}
-
-	checkOut() {
-		this.products.length > 0 && this.delivery !== undefined;
-		if (this.products.length === 0) {
-			throw new Error('Нет продуктов');
-		}
-		if (!this.delivery) {
-			throw new Error('Не выбрали доставку');
-		}
-		return { success: true }
-	}
-}
-
-const cart = new Cart();
-const product1 = new Product(1, 'Печенье', 100);
-const product2 = new Product(2, 'Торт', 300);
-const product3 = new Product(3, 'Молоко', 500);
-cart.addProduct(product1);
-cart.addProduct(product2);
-cart.addProduct(product3);
-cart.removeProduct(2);
-cart.setDelivery(new HomeDelivery(new Date(), 'Москва'));
-cart.finalCost();
-cart.checkOut();
+UserService.getUser(1);
+const inst = new UserService(1);
+inst.create();
