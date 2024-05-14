87da7e80371407c97ffec9d63566dfca67b471dc
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2024-05-14 23:53:39 +0300

    062 Использование в типах

--- build/app.js
+++ build/app.js
@@ -1,24 +1,27 @@
 "use strict";
-function toStringFunc(data) {
-    if (Array.isArray(data)) {
-        return data.toString();
-    }
-    switch (typeof data) {
-        case 'string':
-            return data;
-        case 'number':
-        case 'symbol':
-        case 'bigint':
-        case 'boolean':
-        case 'function':
-            return data.toString();
-        case 'object':
-            return JSON.stringify(data);
-        default:
-            return undefined;
-    }
+function logMiddleware(data) {
+    console.log(data);
+    return data;
 }
-toStringFunc(1);
-toStringFunc(true);
-toStringFunc(['a', 'b']);
-toStringFunc({ a: 1 });
+const res = logMiddleware(10);
+function splitHalf(data) {
+    const length = data.length;
+    return data.splice(0, length / 2);
+}
+const arr = ['test', 'test2', 'test3', 'test4', 'test5', 'test6'];
+const res2 = splitHalf(arr);
+console.log(res2);
+const split = splitHalf;
+const split2 = splitHalf;
+const logLine = {
+    timeStamp: new Date(),
+    data: {
+        a: 1
+    }
+};
+const logLineWithType = {
+    timeStamp: new Date(),
+    data: {
+        a: 1
+    }
+};
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,42 @@
-function toStringFunc<T>(data: T): string | undefined {
-
-	if (Array.isArray(data)) {
-		return data.toString();
-	}
-
-	switch (typeof data) {
-		case 'string':
-			return data;
-		case 'number':
-		case 'symbol':
-		case 'bigint':
-		case 'boolean': 
-		case 'function': 
-			return data.toString();
-		case 'object':
-			return JSON.stringify(data);
-		default:
-			return undefined;
-	}	
+function logMiddleware<T>(data: T): T {
+	console.log(data);
+	return data;
 }
 
-toStringFunc(1);
-toStringFunc(true);
-toStringFunc(['a', 'b']);
-toStringFunc({a: 1});
+const res = logMiddleware<number>(10);
+
+function splitHalf<T>(data: Array<T>): Array<T> {
+	const length = data.length;
+
+	return data.splice(0, length / 2);
+}
+const arr = ['test', 'test2', 'test3', 'test4', 'test5', 'test6'];
+const res2 = splitHalf<string>(arr);
+console.log(res2);
+
+const split: <T>(data: Array<T>) => Array<T> = splitHalf;
+const split2: <Y>(data: Array<Y>) => Array<Y> = splitHalf;
+
+interface ILogLine<T> {
+	timeStamp: Date,
+	data: T
+}
+
+type LogLineType<T> = {
+	timeStamp: Date,
+	data: T
+}
+
+const logLine: ILogLine<{a: number}> = {
+	timeStamp: new Date(),
+	data: {
+		a: 1
+	}
+}
+const logLineWithType: LogLineType<{a: number}> = {
+	timeStamp: new Date(),
+	data: {
+		a: 1
+	}
+}
+
