f88105da6c22f026abf8cb294a271083db7f026e
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2024-05-27 23:54:43 +0300

    069 Упражнение - Пишем функцию группировки

--- build/app.js
+++ build/app.js
@@ -1,10 +1,47 @@
 "use strict";
-const key = 'age';
-function getValue(obj, key) {
-    return obj[key];
+/**
+Необходимо написать функцию группировки, которая принимает массив объектов
+и его ключ, производит группировку по указанному ключу и возращает
+сгруппированный объект.
+Пример:
+``` js
+[
+    { group: 1, name: 'a' },
+    { group: 1, name: 'b' },
+    { group: 2, name: 'c' },
+];
+```
+
+При группироке по 'group' ---->
+
+``` js
+{
+    '1': [ { group: 1, name: 'a' }, { group: 1, name: 'b' } ],
+    '2': [ { group: 2, name: 'c' } ]
 }
-const user = {
-    name: 'Вася',
-    age: 30
-};
-const userName = getValue(user, 'name');
+```
+*/
+const data = [
+    { group: 1, name: 'a' },
+    { group: 1, name: 'b' },
+    { group: 2, name: 'a' }
+];
+function groupArray(arr, key) {
+    let map = new Map();
+    arr.map(item => {
+        for (let k in item) {
+            if (k === key) {
+                map.set(item[key], arr.filter(it => it[key] === item[key]));
+            }
+        }
+    });
+    const obj = {};
+    map.forEach((value, key) => {
+        obj[key] = value;
+    });
+    return obj;
+}
+const obj = groupArray(data, 'name');
+console.log(obj);
+const obj2 = groupArray(data, 'group');
+console.log(obj2);
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,61 @@
-interface IUser {
-	name: string,
-	age: number
+/**
+Необходимо написать функцию группировки, которая принимает массив объектов
+и его ключ, производит группировку по указанному ключу и возращает
+сгруппированный объект.
+Пример:
+``` js
+[
+	{ group: 1, name: 'a' },
+	{ group: 1, name: 'b' },
+	{ group: 2, name: 'c' },
+];
+```
+
+При группироке по 'group' ---->
+
+``` js
+{
+	'1': [ { group: 1, name: 'a' }, { group: 1, name: 'b' } ],
+	'2': [ { group: 2, name: 'c' } ]
+}
+```
+*/
+
+interface IData {
+	group: number;
+	name: string;
 }
 
-type KeysOfUser = keyof IUser;
+const data: IData[] = [
+	{ group: 1, name: 'a' },
+	{ group: 1, name: 'b' },
+	{ group: 2, name: 'a' }
+] 
 
-const key: KeysOfUser = 'age';
-
-function getValue<T, K extends keyof T>(obj: T, key: K) {
-	return obj[key];
+interface IGroup<T extends IData> {
+	[key: string ]: T[]
 }
 
-const user: IUser = {
-	name: 'Вася',
-	age: 30
+function groupArray<T extends Array<IData>, K extends keyof IData>(arr: T, key: K): IGroup<IData> {
+	let map = new Map();
+	arr.map(item => {
+		for (let k in item) {
+			if (k === key) {
+				map.set(item[key], arr.filter(it => it[key] === item[key]));
+			}
+		}
+	})
+
+	const obj: IGroup<IData> = {};
+	map.forEach((value, key) => {
+		obj[key] = value;
+	});
+
+	return obj;
 }
 
-const userName = getValue(user, 'name');
+const obj = groupArray(data, 'name');
+console.log(obj)
+
+const obj2 = groupArray(data, 'group')
+console.log(obj2)
